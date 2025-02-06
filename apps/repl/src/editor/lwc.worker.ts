61728cadd63ad17c4dcfd12766c0f7693d75b904
Matheus Cardoso <matheus@cardo.so>
2025-02-06 20:11:44 -0300

    chore: remove repl (#13)

--- apps/repl/src/editor/lwc.worker.ts
+++
@@ -1,3 +0,0 @@
-// biome-ignore lint/performance/noBarrelFile: <explanation>
-// biome-ignore lint/performance/noReExportAll: <explanation>
-export * from "monaco-editor/esm/vs/language/html/html.worker.js";
